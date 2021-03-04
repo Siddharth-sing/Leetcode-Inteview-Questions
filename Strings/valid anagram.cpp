3314a876baf3bfef1bcf6de6f6a2ff4310f2b7d0
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-03-04 22:19:00 +0530

    Added Map Method

--- Strings/valid anagram.cpp
+++ Strings/valid anagram.cpp
@@ -31,3 +31,24 @@ public:
     return V  ;
   }
 };
+
+/* Map Implementation 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         unordered_map<char, int> map;
+        if(s.size() != t.size())
+            return false; 
+        for(auto i:s)
+            map[i]++;
+        for(auto i:t)
+            map[i]--;
+        for(auto i:map)
+            if(i.second != 0)
+                return false;
+        return true;
+    }
+};
+
+*/
